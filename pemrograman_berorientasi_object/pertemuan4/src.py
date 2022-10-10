5a8cd4701d7714b6cbf0bfdb2b4e3652ee138e4d
tekkheng <tekkheng22@gmail.com>
2022-10-10 21:58:51 +0700

    update

--- pemrograman_berorientasi_object/pertemuan4/src.py
+++ pemrograman_berorientasi_object/pertemuan4/src.py
@@ -17,3 +17,35 @@ def tes(a) :
     umur=int(input("umur: "))
 
 tes("akheng")
+
+
+
+def input_alas_dan_tinggi ():
+  alas = float(input('Masukkan alas: '))
+  tinggi = float((input('Masukkan tinggi: ')))
+
+  return alas, tinggi
+
+def hitung_luas (alas, tinggi):
+  return 0.5 * alas * tinggi
+
+"""kalau fungsional, kita sendiri yang mengelola
+hasil kembaliannya"""
+
+# satu fungsi bisa dipanggil secara independen
+#print(hitung_luas(5, 10))
+
+# contoh dengan inputan alas dan tinggi
+alas, tinggi = input_alas_dan_tinggi()
+print(hitung_luas(alas, tinggi))
+
+
+
+def testing():
+    listCity = ['Kalimantan', 'Sumatra', 'Sulawesi']
+    listPlace = ['West', 'North', 'South']
+    for city in listCity:
+        for place in listPlace:
+            print(city, place)
+
+testing()
