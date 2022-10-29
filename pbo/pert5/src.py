bee67d5baeee744150ddaa0036885e3a70b1da42
tekkheng <tekkheng22@gmail.com>
2022-10-29 21:29:23 +0700

    update

--- pbo/pert5/src.py
+++ pbo/pert5/src.py
@@ -70,4 +70,18 @@
 #====================================================================================================
 
 
+class Product:
+    # __init__ => Merupakan Method Konstruktor
+    # Method untuk Setter
+    def __init__(self,ID_Produk,Nama_Produk,Harga,Jumlah):
+        self.IDProduk = ID_Produk 
+        self.Nama_Produk = Nama_Produk
+        self.Harga = Harga
+        self.Jumlah = Jumlah 
 
+# Isi data dalam method Setter
+a = Product(0,"teh Botol",1323,445)
+
+# Method untuk Getter
+print("Id_Produk : {}\nNama_Produk : {}\nHarga : {}\nJumlah : {} "
+.format( a.IDProduk, a.Nama_Produk, a.Harga, a.Jumlah))
