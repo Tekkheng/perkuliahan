0d6197345c0a13f6d5d06fa99c65c58b676a2545
tekkheng <tekkheng22@gmail.com>
2022-11-02 21:09:03 +0700

    update

---
+++ bahasa_pemrograman/pert6/tugas.py
@@ -0,0 +1,10 @@
+def tugas6():
+    print("="*20)
+    print("PROGRAM QUIZ")
+    print("="*20)
+    nama = input("Masukkan Nama : ")
+    nim = int(input("Masukkan Nim : "))
+    print("")
+    print(f"Nama : {nama}\nNim : {nim}")
+
+tugas6()
