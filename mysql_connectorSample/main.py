68fd24a0140f6f2781c6393f44f72c1f9781e3d1
root <root@Akheng>
2023-01-16 15:15:03 +0700

    update

---
+++ mysql_connectorSample/main.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  port=23306,
+  user="root",
+  password="p455w0rd",
+#   database="Tekkheng_Garden"
+)
+
+db = mydb.cursor()
+
+db.execute("SHOW DATABASES");
+
+try:
+    for i in db:
+        print(i);
+except:
+    print("Gagal!");
